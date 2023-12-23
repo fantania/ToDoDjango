8e2131fd779b1074fb12fd668fe43e54712f5cf7
Fantine NYIRANDIKUBWIMANA <fantine@Fantines-MBP.lan>
2023-12-22 23:56:43 -0500

    Finale

--- todo_main/todo/views.py
+++ todo_main/todo/views.py
@@ -1,5 +1,5 @@
 from django.http import HttpResponse
-from django.shortcuts import redirect, render
+from django.shortcuts import get_object_or_404, redirect, render
 
 from todo.models import Task
 
@@ -8,3 +8,36 @@ def addTask(request):
     task = request.POST['task'] #The input field name='task'
     Task.objects.create(task=task) #Create task in database
     return redirect('home')
+
+def mark_as_done(request,pk):
+    task = get_object_or_404(Task, pk = pk) #first 'pk' is the field name from the database and the second 'pk' is the parameter name
+    task.is_completed = True
+    task.save() #save the task in the database with the update
+    return redirect('home')
+
+def mark_as_undone(request,pk):
+    task = get_object_or_404(Task, pk = pk) #first 'pk' is the field name from the database and the second 'pk' is the parameter name
+    task.is_completed = False
+    task.save() #save the task in the database with the update
+    return redirect('home')
+
+def edit_task(request, pk):
+    task = get_object_or_404(Task, pk = pk)
+    if request.method == 'POST':
+        new_task = request.POST['task'] #The input field name='task'
+        task.task = new_task
+        task.save() #update
+        return redirect('home')
+    else:
+        ctx = {
+        'task':task
+        }
+    
+        return render(request, 'edit_task.html', ctx)
+    
+def delete_task(request,pk):
+    task = get_object_or_404(Task, pk = pk) #first 'pk' is the field name from the database and the second 'pk' is the parameter name
+    task.delete()
+    return redirect('home')
+
+
--- todo_main/todo_main/views.py
+++ todo_main/todo_main/views.py
@@ -5,7 +5,10 @@ from todo.models import Task
 
 def home(request):
     tasks = Task.objects.filter(is_completed = False).order_by('-updated_at') #('-') descending order
+    completed_tasks = Task.objects.filter(is_completed = True).order_by('-updated_at')
     context = {
         'tasks':tasks,
+        'completed_tasks':completed_tasks,
+        
     }
     return render(request,'home.html',context)
